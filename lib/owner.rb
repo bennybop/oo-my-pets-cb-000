cb1f75a846490c05cd43ac06eef43ed5b4521171
bennybop <bennybop74@yahoo.com>
2018-01-14 19:50:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,7 +52,7 @@ class Owner
   end
 
   def sell_pets
-    pets.each do |species, animal|
+    pets.each do |species, animals|
       animals.each do |animal|
         animal.mood = "nervous"
       end
