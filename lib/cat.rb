d14da090238c189ed4fe252e3832526f6e93eb9e
bennybop <bennybop74@yahoo.com>
2018-01-14 19:34:48 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,9 @@
 class Cat
-  attr_writer :mood
-  attr_reader :name
-  def initialize(name, mood)
-    @name = name
-    @mood = mood
+  attr_reader :name 
+  attr_accessor :mood 
 
+  def initialize(name)
+    @name = name 
+    @mood = "nervouse"
   end
 end
